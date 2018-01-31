33448c98c6015c42506b60871fbce6de6079eb10
Valentin Rios <vrios2504@gmail.com>
2018-01-30 20:54:17 -0500

    Models created

--- customers_admin_app/models.py
+++ customers_admin_app/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    flag_path = models.CharField(max_length=200)
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Sport(models.Model):
+    name = models.CharField(max_length=200)
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    name =  models.CharField(max_length=200)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    sports = models.ForeignKey(Sport, on_delete=models.PROTECT)
+    age = models.IntegerField(default=0)
+    added = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
