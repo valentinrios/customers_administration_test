b2fec16956cb782e72d24f26811f071dd5c487b6
Valentin Rios <vrios2504@gmail.com>
2018-01-30 20:32:13 -0500

    customers_admin_app created

---
+++ customers_admin_app/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class CustomersAdminAppConfig(AppConfig):
+    name = 'customers_admin_app'
